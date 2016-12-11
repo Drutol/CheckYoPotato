36d1b0f8a461101999c1edc74510857ae00997b5
Drutol <piotr.szm@poczta.fm>
2016-12-11 02:56:47 +0100

    Simple asp thing + rpi message

---
+++ CheckYoPotato.Web/Controllers/PhotoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using CheckYoPotato.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace CheckYoPotato.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class PhotoController : Controller
+    {
+        public IPhotosRepository Photos { get; set; }
+
+        public PhotoController(IPhotosRepository todoItems)
+        {
+            Photos = todoItems;
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(int id)
+        {
+            var item = Photos.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Photo item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            Photos.Add(item);
+
+            return CreatedAtRoute("GetTodo", new { id = item.camId }, item);
+        }
+    }
+}
---
+++ CheckYoPotato.Web/Repository/PhotosRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheckYoPotato.Web.Models;
+
+namespace CheckYoPotato.Web.Repository
+{
+    public class PhotosRepository : IPhotosRepository
+    {
+        private static ConcurrentDictionary<int, Photo> _todos =
+            new ConcurrentDictionary<int, Photo>();
+
+        public PhotosRepository()
+        {
+            Add(new Photo { Link = "https://checkyopotato.blob.core.windows.net/fridge1/photo.png", Timestamp = DateTime.Now});
+        }
+
+        public IEnumerable<Photo> GetAll()
+        {
+            return _todos.Values;
+        }
+
+        public void Add(Photo item)
+        {
+            _todos[item.camId] = item;
+        }
+
+        public Photo Find(int key)
+        {
+            Photo item;
+            _todos.TryGetValue(key, out item);
+            return item;
+        }
+
+        public Photo Remove(int key)
+        {
+            Photo item;
+            _todos.TryRemove(key, out item);
+            return item;
+        }
+
+        public void Update(Photo item)
+        {
+            _todos[item.camId] = item;
+        }
+    }
+}
