f195c3e248bdddd26048e25c1cd444136f7a9880
Sztecondus <adam.trzeszczak@hotmail.com>
2016-12-11 05:19:25 +0100

    Added properER hamburger items

---
+++ CheckYoPotato.UWP/Converters/HamburgerItemNameToImageSourceConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace CheckYoPotato.UWP.Converters
+{
+    class HamburgerItemNameToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return $"../Assets/images/{value}.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
